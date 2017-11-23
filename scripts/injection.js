3c1d1145f55f3728be316431a997026bf86fe0e9
Aqui Tsuchida <aqui.tcd+dev@gmail.com>
2017-11-23 16:40:16 +0900

    fix(css): use normal css for deploying until resolve injection problem

--- scripts/injection.js
+++ scripts/injection.js
@@ -1,11 +1,11 @@
-const fs = require('hexo-fs');
-const pathFn = require('path');
-hexo.extend.helper.register('ampcss', function(args){
-  let result = '';
-  let path = args;
-  if (path.indexOf('?') < 0 && path.substring(path.length - 4, path.length) !== '.css') path += '.css';
-  fs.readFile(this.url_for(path)).then(function(content){
-    result += '<style amp-custom>' + content + '</style>';
-   })
-  return result;
-}, {async: false});
+// const fs = require('hexo-fs');
+// const pathFn = require('path');
+// hexo.extend.helper.register('ampcss', function(args){
+//   let result = '';
+//   let path = args;
+//   if (path.indexOf('?') < 0 && path.substring(path.length - 4, path.length) !== '.css') path += '.css';
+//   fs.readFile(this.url_for(path)).then(function(content){
+//     result += '<style amp-custom>' + content + '</style>';
+//    })
+//   return result;
+// }, {async: false});
