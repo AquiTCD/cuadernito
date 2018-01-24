e6a7f34e3aad3cb2d43ff3231b9e3ffe7387256d
Aqui Tsuchida <aqui.tcd+dev@gmail.com>
2018-01-24 11:02:35 +0900

    fix related post temporary

--- scripts/related-posts-generator.js
+++ scripts/related-posts-generator.js
@@ -1,49 +1,34 @@
 // Examples of helper
-hexo.extend.helper.register('relatedPostsGenerator', function(args) {
-  if (!args || !args.json || args.json.length === 0) return ''
-  let returnHTML = ''
-  function generateHTML(list) {
-    let ret = ''
-    ret += '<li class="' + args.class + '-item">'
-    if (list.date && list.date !== '') {
-      ret += '<div class="' + args.class + '-date">' + list.date + '</div>'
-    }
-    if (list.img && list.img !== '') {
-      ret +=
-        '<div class="' +
-        args.class +
-        '-img">' +
-        '<img src="' +
-        list.img +
-        '" />' +
-        '</div>'
-    }
-    ret +=
-      '<div class="' +
-      args.class +
-      '-title"><h3><a href="' +
-      list.path +
-      '" title="' +
-      list.title +
-      '" rel="bookmark">' +
-      list.title +
-      '</a></h3></div>'
-    if (list.excerpt && list.excerpt !== '') {
-      ret +=
-        '<div class="' +
-        args.class +
-        '-excerpt"><p>' +
-        list.excerpt +
-        '</p></div>'
+hexo.extend.helper.register('relatedPostsGenerator', function(args){
+  if(!args || !args.json || args.json.length == 0)return '';
+
+  let returnHTML = '';
+
+  function generateHTML(list){
+    let ret = '';
+    ret += '<li class="' + args.class + '-item">';
+
+    if(list.date && list.date != ''){
+        ret += '<div class="'+args.class+'-date">' + list.date + '</div>';
     }
 
-    ret += '</li>'
-    return ret
+    // if(list.img && list.img != ''){
+    //     ret += '<div class="'+args.class+'-img">' + '<img src="'+list.img+'" />' + '</div>';
+    // }
+    ret += '<a href="' + list.path + '" title="'+ list.title +'" rel="bookmark">'+ list.title + '</a>';
+    if(list.excerpt &&  list.excerpt != ''){
+        ret += '<div class="'+args.class+'-excerpt"><p>' + list.excerpt + '</p></div>';
+    }
+
+    ret +=  '</li>';
+    return ret;
   }
-  for (let i = 0; i < args.json.length; i++) {
-    returnHTML += generateHTML(args.json[i])
+
+  for(let i=0; i<args.json.length; i++){
+      returnHTML += generateHTML(args.json[i]);
   }
-  if (returnHTML !== '')
-    returnHTML = '<ul class="' + args.class + '">' + returnHTML + '</ul>'
-  return returnHTML
-})
+
+  if(returnHTML != '')returnHTML = '<div class="relatedPosts_title">関連記事</div><ul class="' + args.class + '">' + returnHTML + '</ul>';
+
+  return returnHTML;
+});
